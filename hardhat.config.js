738801b1ffee309fdde3bed85a5af831eaa4af5d
Andrew Holmes <andrewholmes@Andrews-MacBook-Pro.local>
2021-12-27 22:54:35 -0600

    config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,23 @@
+require("hardhat/config");
+require("@nomiclabs/hardhat-ethers/signers");
+require("ethers");
 require("@nomiclabs/hardhat-waffle");
 
-const FORK_FUJI = false;
-const forkingData = {
-  url: 'https://api.avax-test.network/ext/bc/C/rpc',
-}
+const FORK_FUJI = true;
+const FORK_MAINNET = false;
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+// const forkingData = {
+//   url: 'https://api.avax-test.network/ext/bc/C/rpc',
+// }
+
+const forkingData = FORK_FUJI ? {
+  url: 'https://api.avax-test.network/ext/bc/C/rpc',
+} : FORK_MAINNET ? {
+  url: 'https://api.avax.network/ext/bc/C/rpc'
+} : undefined;
+
+// // This is a sample Hardhat task. To learn how to create your own go to
+// // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -22,7 +33,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.4.23"
+      },
+      {
+        version: "0.8.4"
+      }
+    ]
+  },
   networks: {
     hardhat: {
       gasPrice: 225000000000,
@@ -36,14 +56,6 @@ module.exports = {
       accounts: [
         "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027",
         "0x7b4198529994b0dc604278c99d153cfd069d594753d471171a1d102a10438e07",
-        "0x15614556be13730e9e8d6eacc1603143e7b96987429df8726384c2ec4502ef6e",
-        "0x31b571bf6894a248831ff937bb49f7754509fe93bbd2517c9c73c4144c0e97dc",
-        "0x6934bef917e01692b789da754a0eae31a8536eb465e7bff752ea291dad88c675",
-        "0xe700bdbdbc279b808b1ec45f8c2370e4616d3a02c336e68d85d4668e08f53cff",
-        "0xbbc2865b76ba28016bc2255c7504d000e046ae01934b04c694592a6276988630",
-        "0xcdbfd34f687ced8c6968854f8a99ae47712c4f4183b78dcc4a903d1bfe8cbf60",
-        "0x86f78c5416151fe3546dece84fda4b4b1e36089f2dbc48496faf3a950f16157c",
-        "0x750839e9dbbd2a0910efe40f50b2f3b2f2f59f5580bb4b83bd8c1201cf9a010a"
       ]
     },
     fuji: {
@@ -51,7 +63,7 @@ module.exports = {
       gasPrice: 225000000000,
       chainId: 43113,
       accounts: [
-        "",
+        "fc5982a175f3d6c493880ce717808ee4343c3494722bbdd7b98f39dd5743864e",
       ]
     },
     mainnet: {
